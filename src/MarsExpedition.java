4dd3602c4f8e14c89eca0fbf1903581e0179b1b5
Molly Etnier Muniz <7molly26@pm.me>
2022-08-23 19:59:35 -0500

    re-try Aug 23

--- src/MarsExpedition.java
+++ src/MarsExpedition.java
@@ -1,2 +1,55 @@
-package PACKAGE_NAME;public class MarsExpedition {
+import java.util.Scanner;
+
+public class MarsExpedition {
+    public static void main(String[] args) {
+//        public MarsExpedition(){}
+        Scanner input = new Scanner(System.in);
+        System.out.println("Expedition prep program starting...");
+        System.out.println("...");
+        System.out.println("...");
+        System.out.println("...");
+        System.out.println("Ready");
+        System.out.println("Hello there, what's your name?");
+        String name =input.nextLine();
+        System.out.println("Hi, "+name+". Welcome to the Expedition prep program. Are you ready to head out into the new world? Type Y or N");
+
+        String excited = input.nextLine();
+        if (excited.equalsIgnoreCase("Y")) {
+            System.out.println("I knew you would say that. You are team leader for a reason.");
+        }else{
+            System.out.println("To bad you are team leader. You have to go.");
+        }
+        System.out.println("How many people do you want on your expedition team?");
+        int teamSize = input.nextInt();
+        input.nextLine();
+        if (teamSize>2){
+            System.out.println("That's way to many people. We can only send 2 more members.");
+            teamSize=2;
+        } else if (teamSize<2) {
+            System.out.println("That's not enough people. We need you and 2 more members.");
+            teamSize=2;
+        } else if (teamSize==2) {
+            System.out.println("That's a perfect-sized team.");
+        }
+
+        System.out.println("You are allowed to bring one weapon with you. You know, just in case. What do you want to bring?");
+
+        String weapon = input.nextLine();
+        System.out.println("Nice choice, you'll have "+ weapon+ " at your disposal.");
+        System.out.println("You have the choice of 3 vehicles” “nA: A Mars Rover” “nB: A Chevy Silverado” “nC: A Honda Civic”");
+        String VehicleChoice = input.nextLine();
+        if (VehicleChoice.equalsIgnoreCase("A")){
+            VehicleChoice ="a Mars Rover";
+        } else if (VehicleChoice.equalsIgnoreCase("B")) {
+            VehicleChoice = "a Chevy Silverado";
+        } else if (VehicleChoice.equalsIgnoreCase("C")) {
+            VehicleChoice = "a Honda Civic";
+        }else {
+            VehicleChoice = ("your normal solution for ambulation.");
+        }
+        System.out.println("Your expedition team is now ready, \nLed by"+ name+
+                 " with "+teamSize+" teammates.” “\nTo explore the surface of Mars using"+ VehicleChoice+ ".  \nExploration team heads out in \n5… \n4… \n3… \n2… \n1… \nGO GO GO! \n"
+        );
+
+    }
 }
--- src/guessingGame.java
+++ src/guessingGame.java
@@ -1,2 +1,41 @@
-package PACKAGE_NAME;public class guessingGame {
+import java.util.Random;
+import java.util.Scanner;
+public class guessingGame {
+
+    public guessingGame() {
+
+        Scanner input = new Scanner(System.in);
+        boolean correctGuess = false;
+        int guess = 0;
+        int increment = 0;
+
+        System.out.println("Hello there! What is your name?");
+        String name = input.nextLine();
+        System.out.println("Welcome, " + name + " I'm thinking of a number between 1 and 100.");
+        System.out.println("Try to guess the number!");
+        Random rand = new Random();
+        int number = rand.nextInt(100);
+//        System.out.println("It's "+number);
+
+        while (correctGuess == false) {
+            increment++;
+            try {
+                guess = input.nextInt();
+            } catch (Exception e) {
+                String badInput = input.next();
+                System.out.println("That's not an integer, try again");
+                continue;
+            }
+            if (guess > 100 || guess < 1) {
+                System.out.println("Your guess is out of range.");
+            } else if (guess > number) {
+                System.out.println("Your guess is too high");
+            } else if (guess < number) {
+                System.out.println("Your guess is too low.");
+            } else if (guess == number) {
+                System.out.println("Well done! You found my number in " + increment + " tries!");
+                correctGuess = true;
+            }
+        }
+    }
 }
